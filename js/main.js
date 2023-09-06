de5c058490aae89c6512d71cfd4720658e1d4ef8
Dartsac <idobson.gatech@gmail.com>
2023-09-06 13:32:28 -0400

    formatting

--- js/main.js
+++ js/main.js
@@ -1,37 +1,37 @@
-const primaryNav = document.querySelector('#primary-navigation');
-const navToggle = document.querySelector('.mobile-nav-toggle');
-const primaryNavOptions = document.querySelectorAll('#primary-navigation a');
+const primaryNav = document.querySelector('#primary-navigation')
+const navToggle = document.querySelector('.mobile-nav-toggle')
+const primaryNavOptions = document.querySelectorAll('#primary-navigation a')
 
-navToggle.addEventListener('click', () =>{
-  const visibility = primaryNav.getAttribute('data-visible');
+navToggle.addEventListener('click', () => {
+  const visibility = primaryNav.getAttribute('data-visible')
 
-  if (visibility === "false") {
+  if (visibility === 'false') {
     window.addEventListener('scroll', () => {
-      primaryNav.setAttribute("data-visible", false);
-      navToggle.setAttribute('aria-expanded', false);
-    });
-    primaryNav.setAttribute("data-visible", true);
-    navToggle.setAttribute('aria-expanded', true);
+      primaryNav.setAttribute('data-visible', false)
+      navToggle.setAttribute('aria-expanded', false)
+    })
+    primaryNav.setAttribute('data-visible', true)
+    navToggle.setAttribute('aria-expanded', true)
   } else {
     window.addEventListener('scroll', () => {
-      primaryNav.setAttribute("data-visible", false);
-      navToggle.setAttribute('aria-expanded', false);
-    });
-    primaryNav.setAttribute("data-visible", false);
-    navToggle.setAttribute('aria-expanded', false);
+      primaryNav.setAttribute('data-visible', false)
+      navToggle.setAttribute('aria-expanded', false)
+    })
+    primaryNav.setAttribute('data-visible', false)
+    navToggle.setAttribute('aria-expanded', false)
   }
-});
+})
 
-primaryNavOptions.forEach(element => {
+primaryNavOptions.forEach((element) => {
   element.addEventListener('click', () => {
-    const visibility = primaryNav.getAttribute('data-visible');
+    const visibility = primaryNav.getAttribute('data-visible')
 
-    if (visibility === "false") {
-      primaryNav.setAttribute("data-visible", true);
-      navToggle.setAttribute('aria-expanded', true);
+    if (visibility === 'false') {
+      primaryNav.setAttribute('data-visible', true)
+      navToggle.setAttribute('aria-expanded', true)
     } else {
-      primaryNav.setAttribute("data-visible", false);
-      navToggle.setAttribute('aria-expanded', false);
+      primaryNav.setAttribute('data-visible', false)
+      navToggle.setAttribute('aria-expanded', false)
     }
-  });
-});
+  })
+})
